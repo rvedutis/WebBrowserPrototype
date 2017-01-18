7e1ab67ddd55253b81304e0029cf83b2313c7df5
Andrew Tevington <atevington@workwave.com>
2017-01-18 13:05:17 -0500

    manually releasing COM objects, here be dragons

--- PDFConverter.Web/Default.aspx.cs
+++ PDFConverter.Web/Default.aspx.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading;
 using System.Web.UI;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace PDFConverter.Web
--- PDFConverter/PDFConverter.cs
+++ PDFConverter/PDFConverter.cs
@@ -1,17 +1,11 @@
-﻿using System.Collections.Generic;
-using ASPPDFLib;
+﻿using ASPPDFLib;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 namespace PDFConverter
 {
     public static class PDFConverter
     {
-        private const float pixelToPdf = .75F;
-
-        private static PdfManager manager = new PdfManager
-        {
-            RegKey = "IWezgv+yqt4GIAc213KacCXF1YqRnVGi/TkEGJnd8oBlQMSy/9zzPdk7EzuLOVwZ5X1Vzc5OANZ3"
-        };
-
         public static byte[] Convert(string markup, Dimensions dimensions)
         {
             return ConvertMarkup(new string[] { markup }, dimensions)[0];
@@ -21,6 +15,51 @@ namespace PDFConverter
         {
             return Combine(ConvertMarkup(markupList, dimensions));
         }
+
+        public static byte[] Combine(List<byte[]> pdfs)
+        {
+            var manager = getPdfManager();
+            var combinedDoc = manager.CreateDocument();
+
+            foreach (var pdf in pdfs)
+            {
+                var doc = manager.OpenDocumentBinary(pdf);
+
+                for (int index = 0; index < doc.Pages.Count; index++)
+                {
+                    var graphics = combinedDoc.CreateGraphicsFromPage(doc, index + 1);
+                    var newPage = combinedDoc.Pages.Add(doc.Pages[index + 1].Width, doc.Pages[index + 1].Height);
+
+                    var imageScale = BuildParam(
+                        manager,
+                        new Dictionary<string, object>
+                        {
+                            { "x", 0 },
+                            { "y", 0 },
+                            { "ScaleX", 1 },
+                            { "ScaleY", 1 }
+                        }
+                    );
+
+                    newPage.Canvas.DrawGraphics(graphics, imageScale);
+
+                    Marshal.ReleaseComObject(imageScale);
+                    Marshal.ReleaseComObject(newPage);
+                    Marshal.ReleaseComObject(graphics);
+                }
+
+                Marshal.ReleaseComObject(doc);
+            }
+
+            var docBytes = combinedDoc.SaveToMemory();
+
+            Marshal.ReleaseComObject(combinedDoc);
+            Marshal.ReleaseComObject(manager);
+
+            return docBytes;
+        }
+
+        private const float pixelToPdf = .75F;
 
         private static List<byte[]> ConvertMarkup(string[] markupList, Dimensions dimensions)
         {
@@ -36,57 +75,49 @@ namespace PDFConverter
 
         private static byte[] MarkupToPDF(string markup, Dimensions dimensions)
         {
+            var manager = getPdfManager();
             var doc = manager.CreateDocument();
-
             var page = doc.Pages.Add(dimensions.PageWidth * pixelToPdf, dimensions.PageHeight * pixelToPdf);
 
-            var browserOptions = new Dictionary<string, object>
+            var browserOptions = BuildParam(
+                manager,
+                new Dictionary<string, object>
                 {
                     { "PageHeight", dimensions.RenderHeight * dimensions.Zoom },
                     { "Scripts", false }
-                };
+                }
+            );
 
-            var image = doc.OpenUrl(markup, BuildParam(manager, browserOptions));
+            var image = doc.OpenUrl(markup, browserOptions);
 
-            var imageScale = new Dictionary<string, object>
+            var imageScale = BuildParam(
+                manager,
+                new Dictionary<string, object>
                 {
                     { "x", dimensions.MarginLeft * pixelToPdf },
                     { "y", page.Height - (dimensions.MarginTop * pixelToPdf) - (image.Height * (page.Width / image.Width)) },
                     { "ScaleX", page.Width / image.Width },
                     { "ScaleY", page.Width / image.Width }
-                };
+                }
+            );
 
-            page.Canvas.DrawImage(image, BuildParam(manager, imageScale));
+            page.Canvas.DrawImage(image, imageScale);
 
-            return doc.SaveToMemory();
+            var docBytes = doc.SaveToMemory();
+
+            Marshal.ReleaseComObject(imageScale);
+            Marshal.ReleaseComObject(image);
+            Marshal.ReleaseComObject(browserOptions);
+            Marshal.ReleaseComObject(page);
+            Marshal.ReleaseComObject(doc);
+            Marshal.ReleaseComObject(manager);
+
+            return docBytes;
         }
 
-        public static byte[] Combine(List<byte[]> pdfs)
+        private static PdfManager getPdfManager()
         {
-            var combinedDoc = manager.CreateDocument();
-
-            foreach (var pdf in pdfs)
-            {
-                var doc = manager.OpenDocumentBinary(pdf);
-
-                for (int index = 0; index < doc.Pages.Count; index++)
-                {
-                    var graphics = combinedDoc.CreateGraphicsFromPage(doc, index + 1);
-                    var newPage = combinedDoc.Pages.Add(doc.Pages[index + 1].Width, doc.Pages[index + 1].Height);
-
-                    var imageScale = new Dictionary<string, object>
-                    {
-                        { "x", 0 },
-                        { "y", 0 },
-                        { "ScaleX", 1 },
-                        { "ScaleY", 1 }
-                    };
-
-                    newPage.Canvas.DrawGraphics(graphics, BuildParam(manager, imageScale));
-                }
-            }
-
-            return combinedDoc.SaveToMemory();
+            return new PdfManager { RegKey = "IWezgv+yqt4GIAc213KacCXF1YqRnVGi/TkEGJnd8oBlQMSy/9zzPdk7EzuLOVwZ5X1Vzc5OANZ3" };
         }
 
         private static IPdfParam BuildParam(PdfManager manager, Dictionary<string, object> options)
