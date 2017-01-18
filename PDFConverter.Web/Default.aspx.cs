c621171445915b13f00a65c3a32535d38e1162e7
Andrew Tevington <atevington@workwave.com>
2017-01-18 10:34:44 -0500

    ASPPDF rulez

--- PDFConverter.Web/Default.aspx.cs
+++ PDFConverter.Web/Default.aspx.cs
@@ -1,17 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Web.UI;
-using iTextSharp.text;
-using iTextSharp.text.pdf;
 
 namespace PDFConverter.Web
 {
     public partial class _Default : Page
     {
         private readonly List<Guid> images = new List<Guid>();
-        private int _totalPages;
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -36,26 +32,18 @@ namespace PDFConverter.Web
             };
 
             var pages = content.Split(new[] {"#####NEWPAGE#####"}, StringSplitOptions.None);
-
-            // START OF NEW CODE //
             var pdfs = new List<byte[]>();
-            foreach (var page in pages.Take(1))
+
+            foreach (var page in pages)
             {
                 var pdf = PDFConverter.Convert(page, dimensions);
                 pdfs.Add(pdf);
             }
 
-            var testpdf = pdfs.First();
+            var finalPDf = PDFConverter.Combine(pdfs);
 
-            SendPdfToClient(testpdf);
+            SendPdfToClient(finalPDf);
 
-            /*
-            using (var outputStream = new MemoryStream())
-            {
-                var writer = PdfWriter.GetInstance(testpdf, outputStream);
-                writer.CloseStream = false;
-                SendPdfToClient(outputStream);
-            }*/
         }
 
         private void SendPdfToClient(byte[] pdf)
@@ -68,20 +56,6 @@ namespace PDFConverter.Web
             Response.AppendHeader("Content-Transfer-Encoding", "binary");
             Response.AppendHeader("Content-Length", pdf.Length.ToString());
             Response.BinaryWrite(pdf);
-            Response.Flush();
-            Response.End();
-        }
-
-        private void SendPdfToClient(MemoryStream outputStream)
-        {
-            Response.Buffer = true;
-            Response.ClearHeaders();
-            Response.ClearContent();
-            Response.ContentType = "application/pdf";
-            Response.AppendHeader("Content-Disposition", "inline; filename=Report.pdf");
-            Response.AppendHeader("Content-Transfer-Encoding", "binary");
-            Response.AppendHeader("Content-Length", outputStream.Length.ToString());
-            Response.BinaryWrite(outputStream.ToArray());
             Response.Flush();
             Response.End();
         }
--- PDFConverter/PDFConverter.cs
+++ PDFConverter/PDFConverter.cs
@@ -1,5 +1,5 @@
-﻿using Persits.PDF;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using ASPPDFLib;
 
 namespace PDFConverter
 {
@@ -7,36 +7,26 @@ namespace PDFConverter
     {
         private const float pixelToPdf = .75F;
 
-        public static byte[] Convert(string page, Dimensions dimensions)
+        private static PdfManager manager = new PdfManager
         {
-            if (page == null)
-            {
-                return null;
-            }
+            RegKey = "IWezgv+yqt4GIAc213KacCXF1YqRnVGi/TkEGJnd8oBlQMSy/9zzPdk7EzuLOVwZ5X1Vzc5OANZ3"
+        };
 
-            return Generate(page, dimensions);
-        }
-
-        public static byte[] Generate(string markup, Dimensions dimensions)
+        public static byte[] Convert(string markup, Dimensions dimensions)
         {
-            var manager = new PdfManager
-            {
-                RegKey = "GYlb/v2DbZp5SEU9AfMJwoqj07r/YHR+I9XLiQ3ykzkHH8ftMVaYBR/+twbs3fcRhit+VMCVPj5W"
-            };
+            var doc = manager.CreateDocument();
 
-            using (var doc = manager.CreateDocument())
-            {
-                var page = doc.Pages.Add(dimensions.PageWidth * pixelToPdf, dimensions.PageHeight * pixelToPdf);
+            var page = doc.Pages.Add(dimensions.PageWidth * pixelToPdf, dimensions.PageHeight * pixelToPdf);
 
-                var browserOptions = new Dictionary<string, object>
+            var browserOptions = new Dictionary<string, object>
                 {
                     { "PageHeight", dimensions.RenderHeight * dimensions.Zoom },
                     { "Scripts", false }
                 };
 
-                var image = doc.OpenUrl(markup, BuildParam(manager, browserOptions));
+            var image = doc.OpenUrl(markup, BuildParam(manager, browserOptions));
 
-                var imageScale = new Dictionary<string, object>
+            var imageScale = new Dictionary<string, object>
                 {
                     { "x", dimensions.MarginLeft * pixelToPdf },
                     { "y", page.Height - (dimensions.MarginTop * pixelToPdf) - (image.Height * (page.Width / image.Width)) },
@@ -44,13 +34,40 @@ namespace PDFConverter
                     { "ScaleY", page.Width / image.Width }
                 };
 
-                page.Canvas.DrawImage(image, BuildParam(manager, imageScale));
+            page.Canvas.DrawImage(image, BuildParam(manager, imageScale));
 
-                return doc.SaveToMemory();
-            }
+            return doc.SaveToMemory();
         }
 
-        private static PdfParam BuildParam(PdfManager manager, Dictionary<string, object> options)
+        public static byte[] Combine(List<byte[]> pdfs)
+        {
+            var combinedDoc = manager.CreateDocument();
+
+            foreach (var pdf in pdfs)
+            {
+                var doc = manager.OpenDocumentBinary(pdf);
+
+                for (int index = 0; index < doc.Pages.Count; index++)
+                {
+                    var graphics = combinedDoc.CreateGraphicsFromPage(doc, index + 1);
+                    var newPage = combinedDoc.Pages.Add(doc.Pages[index + 1].Width, doc.Pages[index + 1].Height);
+
+                    var imageScale = new Dictionary<string, object>
+                    {
+                        { "x", 0 },
+                        { "y", 0 },
+                        { "ScaleX", 1 },
+                        { "ScaleY", 1 }
+                    };
+
+                    newPage.Canvas.DrawGraphics(graphics, BuildParam(manager, imageScale));
+                }
+            }
+
+            return combinedDoc.SaveToMemory();
+        }
+
+        private static IPdfParam BuildParam(PdfManager manager, Dictionary<string, object> options)
         {
             var paramString = string.Empty;
             int counter = 1;
