d56b77f03a9390960e840ef42e789a0c022d65d3
Andrew Tevington <atevington@workwave.com>
2017-01-24 09:57:03 -0500

    add windows service

---
+++ PDFConverter.Processor/HtmlFileWatcher.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.ServiceProcess;
+
+namespace PDFConverter.Processor
+{
+    public partial class HtmlFileWatcher : ServiceBase
+    {
+        private string _inPath;
+        private string _outPath;
+        private FileSystemWatcher _fileWatcher;
+
+        public HtmlFileWatcher()
+        {
+            _inPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"in\");
+            _outPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"out\");
+
+            CheckCreateFolder(_inPath);
+            CheckCreateFolder(_outPath);
+
+            _fileWatcher = new FileSystemWatcher();
+            _fileWatcher.Path = _inPath;
+            _fileWatcher.Created += new FileSystemEventHandler(OnFileAdded);
+
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _fileWatcher.EnableRaisingEvents = true;
+        }
+
+        protected override void OnStop()
+        {
+            _fileWatcher.EnableRaisingEvents = false;
+        }
+
+        private static void OnFileAdded(object source, FileSystemEventArgs e)
+        {
+
+        }
+
+        private void CheckCreateFolder(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+    }
+}
---
+++ PDFConverter.Processor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ServiceProcess;
+
+namespace PDFConverter.Processor
+{
+    static class Program
+    {
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+
+            ServicesToRun = new ServiceBase[]
+            {
+                new HtmlFileWatcher()
+            };
+
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
